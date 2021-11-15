e5e258cd5efbfd650028a5cbc491cb4ba61d89f8
unknown <duzhenzhen@byxa.com>
2021-11-15 16:03:03 +0800

    modify test main.tf

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,9 +1,26 @@
+provider "alicloud" {
+  region               = var.region
+}
+
+data "alicloud_zones" "default" {}
+
+resource "alicloud_vpc" "vpc" {
+  vpc_name   = "tf_test_foo"
+  cidr_block = "172.16.0.0/12"
+}
+
+resource "alicloud_vswitch" "vsw" {
+  vpc_id     = alicloud_vpc.vpc.id
+  cidr_block = "172.16.1.0/24"
+  zone_id    = data.alicloud_zones.default.ids[0]
+}
+
 module "app-deployer" {
   source = "../../"
 
   region            = var.region != "" ? var.region : null
-  availability_zone = module.vpc.this_availability_zones[0]
-  vpc_id            = module.vpc.this_vpc_id
+  availability_zone = data.alicloud_zones.default.ids[0]
+  vpc_id            = alicloud_vpc.vpc.id
 
   app_name = "example"
   instance_settings = [
@@ -22,7 +39,7 @@ module "app-deployer" {
       data_disks        = null
       max_bandwidth_out = 100
       security_groups   = null
-      vswitch_id        = module.vpc.this_vswitch_ids[0]
+      vswitch_id        = alicloud_vswitch.vsw.id
       private_ip        = null
       user_data         = null
       temp_files        = ["temp_files/master_template.file"]
@@ -45,7 +62,7 @@ module "app-deployer" {
       data_disks        = null
       max_bandwidth_out = 100
       security_groups   = null
-      vswitch_id        = module.vpc.this_vswitch_ids[0]
+      vswitch_id        = alicloud_vswitch.vsw.id
       private_ip        = null
       temp_files        = ["temp_files/worker_template.file"]
       static_files      = ["static_files/worker_static.file"]
@@ -56,14 +73,14 @@ module "app-deployer" {
   ]
 }
 
-module "vpc" {
-  source = "alibaba/vpc/alicloud"
-
-  region       = var.region != "" ? var.region : null
-  vpc_name     = "my_vpc"
-  vswitch_name = "my_vswitch"
-
-  vswitch_cidrs = [
-    "172.16.1.0/24",
-  ]
-}
+#module "vpc" {
+#  source = "alibaba/vpc/alicloud"
+#
+#  region       = var.region != "" ? var.region : null
+#  vpc_name     = "my_vpc"
+#  vswitch_name = "my_vswitch"
+#
+#  vswitch_cidrs = [
+#    "172.16.1.0/24",
+#  ]
+#}
